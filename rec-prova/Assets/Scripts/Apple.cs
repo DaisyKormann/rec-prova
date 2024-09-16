08b31e9b05f0267cf26251633456de75e09b8f5f
DaisyKormann <161627745+DaisyKormann@users.noreply.github.com>
2024-09-16 09:07:17 -0300

    Variaveis

---
+++ rec-prova/Assets/Scripts/Apple.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEditor;
+using UnityEngine.SocialPlatforms.Impl;
+using Photon.Pun.Demo.PunBasics;
+using static UnityEditor.Progress;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+
+public class Apple : MonoBehaviourPun
+{
+    const float speed = 5f;
+    [SerializeField] int score;
+    Rigidbody2D rigidbody2D;
+
+    public int Score { get => score; set => score = value; }
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        rigidbody2D.velocity = Vector2.down * speed;
+
+        if (transform.position.y < -GameManager.instance.ScreenBounds.y)
+        {
+            photonView.RPC("Destroy", RpcTarget.All);
+        }
+    }
+
+    [PunRPC]
+    void DestroyRPC()
+    {
+        Destroy(gameObject);
+    }
+}
