0b4f4bc42e61bfa5c3e5001729cfb4482109cbda
DaisyKormann <161627745+DaisyKormann@users.noreply.github.com>
2024-09-16 10:21:58 -0300

    array & switch

--- rec-prova/Assets/Scripts/SpawnManager.cs
+++ rec-prova/Assets/Scripts/SpawnManager.cs
@@ -5,7 +5,7 @@ using Photon.Pun;
 
 public class SpawnManager : MonoBehaviourPun
 {
-    string applesPrefab = "Prefabs/Apples";
+    public string[] applesPrefab = { "GoldApple", "GreenApple", "RedApple" };
     float timer;
     const float cooldown = 1;
 
@@ -21,14 +21,21 @@ public class SpawnManager : MonoBehaviourPun
     {
         timer -= Time.deltaTime;
 
+        // Se o temporizador chegar a zero ou menos, instancie uma nova maçã.
         if (timer <= 0)
         {
-            timer = cooldown;
 
-            NetworkManager.instance.Instantiate(applesPrefab, new Vector2(GameManager.instance.ScreenBounds.x, Random.Range(-2, 2)), Quaternion.identity);
-            // ver depois a linha de cima quaternion
+           float appleIndex = Random.Range(0, 1);
+           float appleSelected;
 
+            switch(appleIndex)
+            {
+                case 0.5:
+                    {
 
+                    }
+
+            }
         }
     }
 
@@ -36,23 +43,8 @@ public class SpawnManager : MonoBehaviourPun
 
 /*
 {
-    // Array de strings para armazenar os nomes dos prefabs de maçã que podem ser instanciados.
-
-    // Temporizador que controla o intervalo entre os spawns.
-    // Define o tempo de espera entre cada spawn como 1 segundo.
-
 
     {
-        // Verifica se o cliente atual é o Master Client no Photon.
-        {
-            // Se for o Master Client, chama o método Spawn para instanciar as maçãs.
-        }
-    }
-
-
-    {
-        // Diminui o valor do temporizador baseado no tempo decorrido desde o último frame.
-
         // Se o temporizador chegar a zero ou menos, instancie uma nova maçã.
         {
             // Gera um número aleatório entre 0 e 1 para selecionar o tipo de maçã.
